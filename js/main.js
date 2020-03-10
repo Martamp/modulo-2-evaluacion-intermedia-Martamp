23032ac18bd4586ed33e904fb0346a7461d01017
Martamp <motymont@gmail.com>
2020-03-10 08:41:12 +0100

    first styles and structure

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// const btn = document.querySelector('.js-button');
+
+// btn.addEventListener('click', function1);
+
+// function generateRandomNumber() {}
+
+console.log('hola');
