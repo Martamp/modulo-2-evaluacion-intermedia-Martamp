a5945759696a8fe12e14dc08ea6a1cab097d8a6b
Martamp <motymont@gmail.com>
2020-03-10 09:47:50 +0100

    random number and text functioning

--- js/main.js
+++ js/main.js
@@ -2,21 +2,24 @@
 
 const btn = document.querySelector('.js-button');
 let userNumber = document.querySelector('.js-number');
+const textChange = document.querySelector('.js-text');
 
 function getRandomNumber() {
+  //genero el numero random
   let myRandomNumber = Math.ceil(Math.random() * 100);
-  console.log(myRandomNumber);
-  return myRandomNumber;
-}
-
-function getInputNumber() {
+  console.log('el numero random', myRandomNumber);
+  //Coge el numero que hay en el input
   let userNumberData = userNumber.value;
-  console.log('este es', userNumberData);
+  console.log('ha elegido el ', userNumberData);
+
+  // condiciones paracambiar el h3
+  if (userNumberData === myRandomNumber) {
+    textChange.innerHTML = 'Has ganado campeona!!!';
+  } else if (userNumberData < myRandomNumber) {
+    textChange.innerHTML = 'Demasiado bajo';
+  } else if (userNumberData > myRandomNumber) {
+    textChange.innerHTML = 'Demasiado alto';
+  }
 }
 
-function getFunctions() {
-  getRandomNumber();
-  getInputNumber();
-}
-
-btn.addEventListener('click', getFunctions);
+btn.addEventListener('click', getRandomNumber);
